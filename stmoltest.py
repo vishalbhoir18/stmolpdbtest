1a104c9f099ba465433bfda99820700248481b28
vishalbhoir18 <67859075+vishalbhoir18@users.noreply.github.com>
2022-12-17 09:23:44 +0530

    firsttest

---
+++ stmoltest.py
@@ -0,0 +1,12 @@
+pip install stmol
+import streamlit as st
+import stmol
+
+# Get the PDB ID of the protein
+pdb_id = st.text_input("Enter the PDB ID of the protein:")
+
+# Load the protein structure using stmol
+structure = stmol.from_pdb(pdb_id)
+
+# Visualize the structure using stmol's show_structure function
+st.write(stmol.show_structure(structure))
